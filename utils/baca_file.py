5c6afeb0f8fb7ad59d489c5729b68bbcf72e95d0
Abdullah Azzam <99625317+Zaammm16@users.noreply.github.com>
2025-09-16 20:35:08 +0800

    Create baca_file.py

---
+++ utils/baca_file.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def read_file(uploaded_file):
+    if uploaded_file.name.endswith('.csv'):
+        return pd.read_csv(uploaded_file)
+    elif uploaded_file.name.endswith(('.xlsx', '.xls')):
+        return pd.read_excel(uploaded_file)
+    else:
+        raise ValueError("Format file tidak didukung.")
